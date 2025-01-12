1bf9cf520fb5aca895ea4f5d1f949c9e987078d9
s25098 <s25098@pjwstk.edu.pl>
2025-01-12 04:06:46 +0100

    feat: prototype model added

---
+++ prototype_model.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, mean_absolute_error, precision_score, recall_score, f1_score
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+
+data = pd.read_csv('data/cleaned_data.csv')
+
+X = data.drop(columns=['Status'])
+y = data['Status']
+
+categorical_cols = X.select_dtypes(include=['object']).columns
+numerical_cols = X.select_dtypes(exclude=['object']).columns
+
+numerical_pipeline = Pipeline(steps=[
+    ('scaler', StandardScaler())
+])
+
+categorical_pipeline = Pipeline(steps=[
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_pipeline, numerical_cols),
+        ('cat', categorical_pipeline, categorical_cols)
+    ]
+)
+
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('model', RandomForestClassifier(bootstrap=False, criterion='entropy', max_features=0.9,
+                                     min_samples_leaf=16, min_samples_split=3, n_estimators=100, random_state=42))
+])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, predictions)
+precision = precision_score(y_test, predictions, pos_label='Alive')
+recall = recall_score(y_test, predictions, pos_label='Alive')
+f1 = f1_score(y_test, predictions, pos_label='Alive')
+classification_report = classification_report(y_test, predictions)
+
+print(f"Accuracy: {accuracy:.4f}")
+print(f"Precision: {precision:.4f}")
+print(f"Recall: {recall:.4f}")
+print(f"F1-Score: {f1:.4f}")
+print(classification_report)
